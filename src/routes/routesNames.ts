33926f81bfd1bf57322b8df962a2987a33e6d5da
ignatyskevich <ignatyskevich@gmail.com>
2023-01-23 22:15:32 +0600

    changed initial url

--- src/routes/routesNames.ts
+++ src/routes/routesNames.ts
@@ -1,9 +1,9 @@
 type RoutesKeyType = "HOME" | "POST" | "PET_PROJECTS";
 type RoutesType = Record<RoutesKeyType, string>;
 
-const initialURL = "/super-app";
+const initialURL = "/super-app/";
 export const ROUTES_NAMES: RoutesType = {
-  HOME: `${initialURL}/home`,
-  POST: `${initialURL}/posts`,
-  PET_PROJECTS: `${initialURL}/pet-projects`,
+  HOME: `${initialURL}`,
+  POST: `${initialURL}posts`,
+  PET_PROJECTS: `${initialURL}pet-projects`,
 };
