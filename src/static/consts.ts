d3d7e1db6323afa092b923ff6fd3e285a5930eca
ignatyskevich <ignatyskevich@gmail.com>
2023-01-23 21:48:40 +0600

    added deploy

--- src/static/consts.ts
+++ src/static/consts.ts
@@ -6,6 +6,11 @@ export const myTelegram = `@IgnatYaskevich`;
 export const MyLinks = {
   gitGub: "https://github.com/IgnatYaskevich98",
   telegram: "https://t.me/IgnatYaskevich",
-  cv: "https://www.linkedin.com/feed/update/urn:li:activity:7017796422804627456/",
+  cv: "https://www.linkedin.com/posts/ignat-yaskevich-968279218_cv-activity-7023269750004723712-9IUM?utm_source=share&utm_medium=member_desktop",
   linkedin: "https://www.linkedin.com/in/ignat-yaskevich-968279218/",
+};
+
+export const Description: Record<"me" | "app", string> = {
+  me: "I have experience building a SPA using React, Redux, HTML, CSS. Support for existing functionality or development. Worked with API requests,  Swagger, Grafana, Figma. Worked in a large company and interacted and worked with more than 10 developers from different teams. Optimized web apps performance using React. I had experience in business analysis. Practical knowledge of SCRUM. Always strive to learn new skills, take on additional responsibilities and grow professionally.",
+  app: "I decided to use this project to show you my work. I will be to add more information, examples and update regularly. This project has links to my projects in the github repositories, there are also projects that you can see here. These are mini projects to see how I can code. I have attached github links to these mini projects. I want to update this project regularly, and when I learn something new, I will add my new knowledge here and you can see it. This is the main goal of this project.",
 };
