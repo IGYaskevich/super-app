88f2bb5a50bbe56028930a7c87bf79799097d993
ignatyskevich <ignatyskevich@gmail.com>
2023-02-02 01:01:38 +0600

    added new per-project

--- src/static/icons.ts
+++ src/static/icons.ts
@@ -1,28 +1,31 @@
-import { ReactComponent as louder } from "./icons/louder.svg";
-import { ReactComponent as gitGub } from "./icons/github.svg";
-import { ReactComponent as telegram } from "./icons/telegram.svg";
-import { ReactComponent as linkedin } from "./icons/linkedin.svg";
-import { ReactComponent as cv } from "./icons/cv.svg";
-import { ReactComponent as pen } from "./icons/pen.svg";
-import { ReactComponent as pokemon } from "./icons/pokemon.svg";
-import { ReactComponent as githubCircle } from "./icons/github-circle.svg";
-import { ReactComponent as moreRepo } from "./icons/more-svgrepo.svg";
-import { ReactComponent as listPost } from "./icons/list.svg";
-import { ReactComponent as onlineShopping } from "./icons/online-shopping.svg";
+import {ReactComponent as louder} from "./icons/louder.svg";
+import {ReactComponent as gitGub} from "./icons/github.svg";
+import {ReactComponent as telegram} from "./icons/telegram.svg";
+import {ReactComponent as linkedin} from "./icons/linkedin.svg";
+import {ReactComponent as cv} from "./icons/cv.svg";
+import {ReactComponent as pen} from "./icons/pen.svg";
+import {ReactComponent as pokemon} from "./icons/pokemon.svg";
+import {ReactComponent as githubCircle} from "./icons/github-circle.svg";
+import {ReactComponent as moreRepo} from "./icons/more-svgrepo.svg";
+import {ReactComponent as listPost} from "./icons/list.svg";
+import {ReactComponent as onlineShopping} from "./icons/online-shopping.svg";
+import {ReactComponent as nextJS} from "./icons/app-next.svg";
 
 export const ICONS = {
-  louder,
-  pen,
-  pokemon,
-  githubCircle,
-  moreRepo,
-  listPost,
-  onlineShopping,
+    louder,
+    pen,
+    pokemon,
+    githubCircle,
+    moreRepo,
+    listPost,
+    onlineShopping,
+    nextJS,
 };
 
 export const ICONS_LINK = {
-  gitGub,
-  telegram,
-  cv,
-  linkedin,
+    gitGub,
+    telegram,
+    cv,
+    linkedin,
+
 };
